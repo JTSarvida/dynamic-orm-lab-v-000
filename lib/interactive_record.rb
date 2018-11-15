d5372b3642666ec905ed6fe3431651352994461c
Joshua Tan <jtsarvida@gmail.com>
2018-11-15 08:30:52 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,9 @@ class InteractiveRecord
   def table_name_for_insert
     self.class.table_name
   end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 end
